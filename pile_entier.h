5f4bb48d9abc444bcab070acd4555745d2900eb4
Aissatou28 <100479323+Aissatou28@users.noreply.github.com>
2022-10-05 10:49:29 +0200

    Commit2

---
+++ pile_entier.h
@@ -0,0 +1,19 @@
+#pragma once
+class pile_entier
+{
+private:
+	int * pile;
+	int taille; 
+	int hauteur;
+
+public:
+	pile_entier(int size);
+	~pile_entier();
+
+	void empile(int x);
+	int depile();
+	
+	bool pleine();
+	bool vide();
+
+};
